dc52f0e7c4d55267f4d43996c301aafe5574a29a
hattagh73two <hattagh73two@gmail.com>
2023-03-13 00:35:59 +0800

    ADD: index-of-components

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+//* IMPORT: layout-components
+export { default as Layout } from './Layout';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+
+// IMPORT: section-components
+export { default as  SHero } from './sections/SHero';
+export { default as  SProduct } from './sections/SProduct';
+export { default as  SLocation } from './sections/SLocation';
+
+//* IMPORT: reusable-components
+export { default as Container } from './Container';
+
+
