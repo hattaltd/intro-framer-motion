ae588ef273c43c1fa3b9bc6c2dd15257ab3f2d9b
hattagh73two <hattagh73two@gmail.com>
2023-03-13 00:36:07 +0800

    ADD: layout

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+//* START: import-packages-components-etc
+import { Header, Footer } from './index';
+import { ReactNode } from 'react';
+
+//* START: data-types
+type ChildrenProps = {
+    children?: ReactNode
+}
+
+// START: component-layout
+const Layout = ({ children }:ChildrenProps) => {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    )
+}
+export default Layout;
